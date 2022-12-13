2185389dbdb9026ad2b2bfd51c4c76d040fc9dd1
aditya patil <Aditya@example.com>
2022-12-13 12:09:59 +0530

    [Aditya]Add:Find Mximum Integer

--- FindMaxGeneric/Program.cs
+++ FindMaxGeneric/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxGeneric
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = FindMaximum.FindIntMax(100, 500, 1000);
+            Console.WriteLine(max);
+        }
+    }
+}
